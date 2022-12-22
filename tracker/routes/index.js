6ff8ddf037fd95e86b7c45e0055c67df1c3de03e
username <username123@example.com>
2022-12-21 23:01:48 -0500

    make tracker return bytes without conversion

--- tracker/routes/index.js
+++ tracker/routes/index.js
@@ -4,11 +4,24 @@ let controller = require('../model/controller');
 
 /* GET home page. */
 router.get('/announce', function (req, res, next) {
-  console.log('announce call: ',req.query);
+  console.log('announce call: ', req.query);
   const ip = req.socket.remoteAddress;
 
-  const msg = controller.announce(req.query,ip);
-  res.type('text/plain').send(msg);
+  const msg = controller.announce(req.query, ip);
+  let b = Buffer.from(msg,'latin1');
+  res.type('text/plain').send(b);
 });
+
+router.get('/test', function (req, res, next) {
+  let ar = [107, 0, 0, 1, 102, 156,220,255];
+  let msg = '';
+  for (let c of ar) {
+    let cc = String.fromCharCode(c);
+    msg += cc;
+  }
+  let b = Buffer.from(msg,'latin1');
+  res.type('text/plain').send(b);
+});
+
 
 module.exports = router;
